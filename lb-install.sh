461f83a9841e9f9dbbac65d0f9c1d93f77af5f39
bruce-neveruse <bruce.neveruse@gmail.com>
2021-11-12 17:35:30 -0500

    Update lb-install.sh

--- lb-install.sh
+++ lb-install.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 
 gcloud compute backend-services create backend \
     --protocol=HTTPS \
     --port-name=https \
     --health-checks=healthcheck \
+    --global
+
+gcloud compute backend-services add-backend backend \
+    --instance-group=lb-backend-example \
+    --instance-group-zone=us-east4-c \
     --global
