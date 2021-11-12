1861e7ed106c5144169b422d8e727b296286a41e
bruce-neveruse <bruce.neveruse@gmail.com>
2021-11-12 18:28:55 -0500

    Update lb-install.sh

--- lb-install.sh
+++ lb-install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-gcloud compute --project=$PROJECT backend-services create backend \
+gcloud compute backend-services create backend \
     --protocol=HTTPS \
     --port-name=https \
     --health-checks=healthcheck \
     --global
 
-gcloud compute --project=$PROJECT backend-services add-backend backend \
+gcloud compute backend-services add-backend backend \
     --instance-group=web-server-instance-group-use4 \
     --instance-group-zone=us-east4-c \
     --global
 
-gcloud compute --project=$PROJECT url-maps create web-map-https \
+gcloud compute url-maps create web-map-https \
     --default-service backend
 
 # Create Cert
-gcloud compute --project=$PROJECT ssl-certificates create hmlb-cert \
+gcloud compute ssl-certificates create hmlb-cert \
     --domains=hmlb.ddns.net \
     --global
 
-gcloud compute --project=$PROJECT target-https-proxies create https-lb-proxy \
+gcloud compute target-https-proxies create https-lb-proxy \
     --url-map=web-map-https \
     --ssl-certificates=hmlb-cert    
 
-gcloud compute --project=$PROJECT forwarding-rules create https-content-rule \
+gcloud compute forwarding-rules create https-content-rule \
     --address=lb-ipv4-1 \
     --global \
     --target-https-proxy=https-lb-proxy \
