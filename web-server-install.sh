1b852c4417ca0c2fc4aaf1cf73d38b6af9611267
bruce-neveruse <bruce.neveruse@gmail.com>
2021-11-12 11:09:41 -0500

    Update web-server-install.sh

--- web-server-install.sh
+++ web-server-install.sh
@@ -16,5 +16,5 @@ gcloud beta compute --project $PROJECT instance-groups managed set-autoscaling "
 #CLOUD ARMOUR POLICY
 gcloud compute --project=$PROJECT security-policies create hm-access --description="only allow 157.154.3.140, 167.164.3.140, and lab proxy 157.154.3.241"                                                 
 gcloud compute --project=$PROJECT security-policies rules create 0 --action=allow --security-policy=hm-access --src-ip-ranges=157.154.3.140,167.164.3.140,157.154.3.241                                   
-gcloud compute --project=$PROJECT security-policies rules create 2147483647 --action=deny\(403\) --security-policy=hm-access --description="Default rule, higher priority overrides it" --src-ip-ranges=\*
+gcloud compute --project=$PROJECT security-policies rules create 2147483647 --action=deny-403 --security-policy=hm-access --description="Default rule, higher priority overrides it" --src-ip-ranges=\*
 gcloud compute --project=$PROJECT backend-services update backend --security-policy=hm-access
