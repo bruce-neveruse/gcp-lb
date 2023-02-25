8e14565e0d4872e073d128fa15e05cb66fbc3669
Bruce Long <bruce.neveruse@gmail.com>
2023-02-24 19:41:09 -0500

    Update create vpc.tf

--- create vpc.tf
+++ create vpc.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc-network1" {
   name                            = var.vpc_network1
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
-  delete_default_routes_on_create = true
+  delete_default_routes_on_create = false
 }
 
 # === ADD SUBNET1 ===
@@ -19,7 +19,7 @@ resource "google_compute_network" "vpc-network2" {
   name                            = var.vpc_network2
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
-  delete_default_routes_on_create = true
+  delete_default_routes_on_create = false
 }
 
 # === ADD SUBNET1 ===
@@ -35,7 +35,7 @@ resource "google_compute_network" "vpc-network3" {
   name                            = var.vpc_network3
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
-  delete_default_routes_on_create = true
+  delete_default_routes_on_create = false
 }
 
 # === ADD SUBNET1 ===
