50524d7727bda99243fe651a861015c27f7ec549
Bruce Long <bruce.neveruse@gmail.com>
2023-02-24 21:09:54 -0500

    Update cloud-armour-install.sh

--- cloud-armour-install.sh
+++ cloud-armour-install.sh
@@ -9,7 +9,7 @@ gcloud config set project $PROJECT
 
 #CLOUD ARMOUR POLICY
 gcloud compute security-policies create hm-access --description="only allow 157.154.3.140, 167.164.3.140, and lab proxy 157.154.3.241"
-gcloud compute security-policies rules create 0 --action=allow --security-policy=hm-access --src-ip-ranges=157.154.3.140,167.164.3.140,157.154.3.241,152.208.2.117
+gcloud compute security-policies rules create 0 --action=allow --security-policy=hm-access --src-ip-ranges=157.154.3.140,167.164.3.140,157.154.3.241,152.208.38.7
 gcloud compute security-policies rules update 2147483647 --action=deny-403 --security-policy=hm-access --description="Default rule, higher priority overrides it" --src-ip-ranges=\*
 gcloud compute backend-services update backend --security-policy=hm-access --global
 gcloud compute backend-services update backend-app1 --security-policy=hm-access --global
